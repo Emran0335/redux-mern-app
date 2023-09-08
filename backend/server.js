ec752adacd6306c9ece9e21faa2ee7e4220a0ce3
Emran <emranhossain0335@gmail.com>
2023-09-09 00:24:30 +0600

    backend is going to be completed

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log("first")
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware to get body data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+// errorhandler middleware. It will ride over the express error message
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
